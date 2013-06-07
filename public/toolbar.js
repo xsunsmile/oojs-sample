9851162cf8461fec15dc374aa59862a6c71e95c8
Hao Sun <xsunsmile@gmail.com>
2013-06-06 18:47:07 -0700

    add packages

--- public/toolbar.js
+++ public/toolbar.js
@@ -1,6 +1,15 @@
 var oojs = (function(oojs){
   oojs.createToolbar = function(id) {
-    console.log('add toolbar to id: ' + id);
+    var body = document.childNodes[0].childNodes[1];
+    var toolbar = document.getElementById(id);
+    if( !toolbar ) {
+      toolbar = document.createElement("div");
+      toolbar.id = id;
+      toolbar.className = 'toolbar';
+      body.appendChild(toolbar);
+    }
   };
+
+  return oojs;
 }(oojs || {}));
 
